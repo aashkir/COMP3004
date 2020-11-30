0654e06c1c5ebab72b2de273a080f75cf0ad1724
aashkir <ahmedaashkir@gmail.com>
2020-11-30 13:51:50 -0500

    removed redundant folder

--- AddDeck/styles/fonts.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native'
-
-export const fonts = StyleSheet.create({
-    default: { fontSize: 24 },
-    alternate: { fontSize: 50, color: "#FFFFFF" },
-    large: {fontSize: 32, alignSelf: "center" }
-})
-
-export const scalingFactors = { default: 15, large: 10}
